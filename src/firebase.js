e4018c214c30f42985d4e75d567a835af6849ab9
temmuzcetiner <temmuzcetiner@gmail.com>
2026-02-18 10:42:42 +0100

    fix: correct storage bucket URL in firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyCEnUhLvqbZTFfLFhsDUUORyWbYFHG0V18",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "portfolio-b0e27.firebaseapp.com",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "portfolio-b0e27",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "gs://portfolio-b0e27.appspot.com",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "portfolio-b0e27.appspot.com",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "247723789432",
   appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:247723789432:web:59e198796f96dee415f18d"
 };
